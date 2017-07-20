12fc9a79645113d3dd95419d17361ac7f94d2d5d
libowen <libowen@cct.cn>
2017-07-20 14:23:25 +0800

    first commit

---
+++ CellDemo/Helper.swift
@@ -0,0 +1,9 @@
+//
+//  Helper.swift
+//  CellDemo
+//
+//  Created by libowen on 2017/7/20.
+//  Copyright © 2017年 libowen. All rights reserved.
+//
+
+import Foundation
---
+++ CellDemo/ListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListCell.swift
+//  CellDemo
+//
+//  Created by libowen on 2017/7/20.
+//  Copyright © 2017年 libowen. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
