299596532359483d95d6d508a79485c76c9f63cb
libowen <libowen@cct.cn>
2017-11-22 18:28:41 +0800

    调整选中状态view，使用backgroundView

--- CellDemo/FindFriendHelpList/CustomHeaderOfFind.swift
+++ CellDemo/FindFriendHelpList/CustomHeaderOfFind.swift
@@ -10,7 +10,10 @@
 /// 2、添加xib并设置UI
 /// 3、创建代理协议，用于处理open、close逻辑
 /// 4、实现点击section header的hilighted效果：
+///    indicator image：展开section时设置button的seleted状态的image为展开图片
 ///    添加long press gesture；修改自定义view的backgroundColor并添加动画；
+///    根据disclosureButton.isSelected状态调用代理方法
+///
 ///
 
 import UIKit
@@ -25,11 +28,13 @@ import UIKit
 class CustomHeaderOfFind: UITableViewHeaderFooterView {
     
     
+    @IBOutlet weak var xibBackgroundView: UIView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var disclosureButton: UIButton!
 
     
     var section: Int?
+    var isSelected: Bool!
     var delegate: SectionHeaderViewDelegate?
     
     
@@ -46,8 +51,12 @@ class CustomHeaderOfFind: UITableViewHeaderFooterView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-    
+        print("awakeFromNib")
+        
         // setup UI
+        xibBackgroundView.backgroundColor = UIColor.clear
+        // 设置disclosureButton选中情况下的image为展开图像
+        disclosureButton.setImage(UIImage(named: "carat-open.png"), for: .selected)
     
         // add tap gesture for section header
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleOpen))
@@ -63,9 +72,15 @@ class CustomHeaderOfFind: UITableViewHeaderFooterView {
     
     // handle long press
     func handleLongPress(recognizier: UIGestureRecognizer) {
+        // 切换disclosureButton的选中状态
+        //disclosureButton.isSelected = !disclosureButton.isSelected
+        print("disclosureButton.isSelected: \(disclosureButton.isSelected)")
         
-        if let sectionHeader: CustomHeaderOfFind = recognizier.view as? CustomHeaderOfFind {
-            print("long press: \(String(describing: sectionHeader))")
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.clear
+        
+        if let _: CustomHeaderOfFind = recognizier.view as? CustomHeaderOfFind {
+            //print("long press: \(String(describing: sectionHeader))")
             
             if recognizier.state == .began {
                 print("begin")
@@ -73,23 +88,58 @@ class CustomHeaderOfFind: UITableViewHeaderFooterView {
                 /// 实现section header的hilighted效果
                 // 设置hilighted颜色
                 /// 直接修改背景色效果不好，可以添加动画美化效果
+                self.backgroundView = backgroundView
                 UIView.animate(withDuration: 0.1, delay: 0.2, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                    self.subviews[1].backgroundColor = UIColor.lightGray
+                    // 修改自定义xib的背景view的背景色
+                    //self.subviews[1].backgroundColor = UIColor.lightGray
+                    let customBackgroundView = self.subviews[1]
+                    customBackgroundView.backgroundColor = UIColor.lightGray
+                    customBackgroundView.alpha = 0.6
+                
+//                    self.backgroundView?.alpha = 0.6
+//                    self.backgroundView?.backgroundColor = UIColor.lightGray
+                    
                 }, completion: nil)
                 
             } else if recognizier.state == .ended {
                 print("ended")
+                // 切换disclosureButton的选中状态
+                disclosureButton.isSelected = !disclosureButton.isSelected
                 // 取消hilighted颜色
                 UIView.animate(withDuration: 0.1, delay: 0.2, options: UIViewAnimationOptions.curveEaseOut, animations: {
-                    self.subviews[1].backgroundColor = UIColor.white
-                }, completion: nil)
+                    //
+                    //self.subviews[1].backgroundColor = UIColor.white
+                    let customBackgroundView = self.subviews[1]
+                    customBackgroundView.backgroundColor = UIColor.white
+                    customBackgroundView.alpha = 1.0
+                    
+//                    self.backgroundView?.alpha = 0.0
+//                    self.backgroundView?.backgroundColor = UIColor.white
+                    
+                }, completion: { (complete) in
+                    if complete {
+                        //self.backgroundView = nil
+                    }
+                })
                 
-                // 调用代理方法
-                if (self.delegate?.responds(to: #selector(SectionHeaderViewDelegate.sectionHeaderView(_:sectionOpened:))))! {
-                    self.delegate?.sectionHeaderView!(self, sectionOpened: self.section!)
+                //
+                if disclosureButton.isSelected {
+                    // 调用代理方法：展开section
+                    if (self.delegate?.responds(to: #selector(SectionHeaderViewDelegate.sectionHeaderView(_:sectionOpened:))))! {
+                        self.delegate?.sectionHeaderView!(self, sectionOpened: self.section!)
+                    } else {
+                        print("not respond delegate")
+                    }
                 } else {
-                    print("not respond delegate")
+                    // 调用代理方法：收起section
+                    print("call sectionClosed")
+                    if (self.delegate?.responds(to: #selector(SectionHeaderViewDelegate.sectionHeaderView(_:sectionClosed:))))!{
+                        self.delegate?.sectionHeaderView!(self, sectionClosed: self.section!)
+                    } else {
+                        print("not respond delegate")
+                    }
                 }
+                
             }
         }
     }
--- CellDemo/FindFriendHelpList/FindFriendHelpListVC.swift
+++ CellDemo/FindFriendHelpList/FindFriendHelpListVC.swift
@@ -83,10 +83,13 @@ class FindFriendHelpListVC: UITableViewController, SectionHeaderViewDelegate {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        if self.lists[section].isExpanded {
-            return (self.lists[section].friends?.count)!
-        }
-        return 0
+//        if self.lists[section].isExpanded {
+//            return (self.lists[section].friends?.count)!
+//        }
+//        return 0
+        let item = self.lists[section]
+        let numberOfFriends = item.friends?.count
+        return item.isExpanded ? numberOfFriends! : 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -95,18 +98,25 @@ class FindFriendHelpListVC: UITableViewController, SectionHeaderViewDelegate {
         // Configure the cell...
         let frend = self.lists[indexPath.section].friends?[indexPath.row]
         cell.textLabel?.text = frend?.name
-        if indexPath.section == 0 {
-            cell.imageView?.image = nil
-        } else {
-            cell.imageView?.image = UIImage(named: "国内游@2x")
-        }
+//        if indexPath.section == 0 {
+//            cell.imageView?.image = nil
+//        } else {
+//            cell.imageView?.image = UIImage(named: "国内游@2x")
+//        }
+        // add backgroundView
+//        let backgroundView = UIView()
+//        backgroundView.backgroundColor = UIColor.orange
+//        cell.backgroundView = backgroundView
+        // add accessoryType
+        cell.accessoryType = .disclosureIndicator
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        print("viewForHeaderInSection, section: \(section)")
+        
         let sectionHeader: CustomHeaderOfFind = tableView.dequeueReusableHeaderFooterView(withIdentifier: kSectionHeaderIdentifier) as! CustomHeaderOfFind
-        sectionHeader.delegate = self
         
         /// 问题：
         /// [CellDemo.CustomHeaderOfFind handleLongPressWithRecognizier:]: unrecognized selector sent to instance 0x7fca59d4fd60'???
@@ -117,12 +127,17 @@ class FindFriendHelpListVC: UITableViewController, SectionHeaderViewDelegate {
 //        sectionHeader.addGestureRecognizer(longPressGesture)
         
         let item = self.lists[section]
-        sectionHeader.disclosureButton.setImage(UIImage.init(named: item.imageString), for: .normal)
+        //sectionHeader.disclosureButton.setImage(UIImage.init(named: item.imageString), for: .normal)
         sectionHeader.titleLabel.text = item.relations
         sectionHeader.section = section
+        //sectionHeader.isSelected = item.isExpanded
+        
+        sectionHeader.delegate = self
+        
         return sectionHeader
     }
  
+    
     
     // MARK: Table view delegate
     
@@ -137,33 +152,69 @@ class FindFriendHelpListVC: UITableViewController, SectionHeaderViewDelegate {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("")
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
-    // MARK: SectionHeaderViewDelegate
     
+    // MARK: SectionHeaderViewDelegate
+    // 展开section方法
     func sectionHeaderView(_ sectionHeaderView: CustomHeaderOfFind, sectionOpened: Int) {
         print("opened section: \(sectionOpened)")
         
         let item = self.lists[sectionOpened]
         
-        // 切换indicator image
-        /// 注意：这种实现方式列表动作先于图片动作
-        if item.isExpanded {
-            item.imageString = "carat.png"
-        } else {
-            item.imageString = "carat-open.png"
-        }
-        // 转换展开收起状态
-        item.isExpanded = !item.isExpanded
+        // 展开
+        item.isExpanded = true
+        
         // 更新section
-        self.tableView.reloadSections(IndexSet.init(integer: sectionOpened), with: .none)
+        //self.tableView.reloadSections(IndexSet.init(integer: sectionOpened), with: .none)
+        
+        // Create an array containing the index paths of the rows to insert:
+        var indexPathsToInsert: [IndexPath] = []    // empty array
+        guard let countOfRowsToInsert = item.friends?.count else { return }
+        for i in 0..<countOfRowsToInsert {
+            let indexPath = IndexPath(row: i, section: sectionOpened)
+            indexPathsToInsert.append(indexPath)
+        }
+        
+        // style the animation so that there's a smooth flow in either direction
+        let insertAnimation: UITableViewRowAnimation = UITableViewRowAnimation.automatic
+        
+        // apply the updates
+//        self.tableView.beginUpdates()
+//        self.tableView.insertRows(at: indexPathsToInsert, with: insertAnimation)
+//        self.tableView.endUpdates()
+        self.tableView.insertRows(at: indexPathsToInsert, with: insertAnimation)
     }
     
+    // 收起section方法
     func sectionHeaderView(_ sectionHeaderView: CustomHeaderOfFind, sectionClosed: Int) {
         print("closed section: \(sectionClosed)")
+        
+        let item = self.lists[sectionClosed]
+        
+        // 展开
+        item.isExpanded = false
+    
+        // Create an array containing the index paths of the rows to insert:
+        var indexPathsToDelete: [IndexPath] = []
+        guard let countOfRowsToInsert = item.friends?.count else { return }
+        for i in 0..<countOfRowsToInsert {
+            let indexPath = IndexPath(row: i, section: sectionClosed)
+            indexPathsToDelete.append(indexPath)
+        }
+        
+        // style the animation so that there's a smooth flow in either direction
+        let insertAnimation: UITableViewRowAnimation = UITableViewRowAnimation.bottom
+        
+        // apply the updates
+        self.tableView.beginUpdates()
+        self.tableView.deleteRows(at: indexPathsToDelete, with: insertAnimation)
+        self.tableView.endUpdates()
     }
+    
     
     
     // MARK: Helper
@@ -174,6 +225,21 @@ class FindFriendHelpListVC: UITableViewController, SectionHeaderViewDelegate {
         print("long press: \(String(describing: sectionHeader))")
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
